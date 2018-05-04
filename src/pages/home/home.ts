04b956f9b055c26fb136ab2464ec393bb738d299
peterennis <peterennis@yahoo.com>
2018-05-03 21:13:24 -0700

    kute ae animation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+declare var KUTE: any;
 
 @Component({
   selector: 'page-home',
@@ -6,4 +8,31 @@ import {Component} from '@angular/core';
 })
 export class HomePage {
 
+  constructor( ) { }
+
+  drawAdaept ( ) {
+    var tweenAdaept = KUTE.to('#a', { svgTransform: { translate: 580, rotate: 360 } }, { yoyo: true, repeat: 1, duration: 1500, easing: "easingCubicOut"});
+    tweenAdaept.start();
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SvgPage');
+  }
+
 }
+
+
+//import { IonicPageModule } from 'ionic-angular';
+//import { SvgPage } from './svg';
+//import { KUTE } from '../../assets/js/kute-svg.min';
+
+//@NgModule({
+// declarations: [
+//    SvgPage,
+//    //KUTE
+//  ],
+//  imports: [
+//    IonicPageModule.forChild(SvgPage)
+//  ],
+//})
+//export class SvgPageModule {}
